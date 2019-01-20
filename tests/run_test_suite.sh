58a57c119b49c807d44360c021c15a2a26345578
Ori Hoch <ori@uumpa.com>
2019-01-20 15:21:22 +0200

    minor test suite improvements + support for multiple test accounts

--- tests/run_test_suite.sh
+++ tests/run_test_suite.sh
@@ -6,23 +6,25 @@ if [ "${1}" == "" ]; then
 fi
 
 export NUM_SINGLE_MACHINE_TESTS_TO_RUN="${1}"
+TEST_RUNNER_SUFFIX="${2}"
 
 ! [ -e tests/docker-machine-driver-kamatera ] &&\
     echo please follow CONTRIBUTING.md to build a tests image and run this script from the project root &&\
     exit 1
 
 echo Creating a machine to run the test suite from
-! docker-machine --debug create -d kamatera ktm-test-runner && exit 1
-eval "$(docker-machine env ktm-test-runner)"
+! docker-machine --debug create -d kamatera ktm-test-runner${TEST_RUNNER_SUFFIX} && exit 1
+eval "$(docker-machine env ktm-test-runner${TEST_RUNNER_SUFFIX})"
 
 echo Building and running the test suite
 
-export RESULTS_DIRECTORY=/kamatera_test_results
-export SUITE_RUN_TITLE="kamatera-suite-1"
+export RESULTS_DIRECTORY=/kamatera_test${TEST_RUNNER_SUFFIX}_results
+export SUITE_RUN_TITLE="kamatera-suite${TEST_RUNNER_SUFFIX}"
 export MAX_PARALLEL_SINGLE_MACHINE_TESTS=10
+export GLOBAL_TIMEOUT_SECONDS=36000
 
 docker build -t tests tests/ &&\
-docker run -d \
+docker run -d --name test-runner${TEST_RUNNER_SUFFIX} \
            -v /var/run/docker.sock:/var/run/docker.sock \
            -v "${RESULTS_DIRECTORY}/${SUITE_RUN_TITLE}/:/test_results/" \
            -e KAMATERA_API_CLIENT_ID \
